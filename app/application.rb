f835006ca36b26eacb2a8b25d71510f09e48318c
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-19 21:36:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         end
       end
     elsif req.path.match(/add/)
-      item = req.params["items"]
+      item = req.params["item"]
       binding.pry
       if @@items.include?(item)
 
