e0c32f3ab6cbe7aefb916fe0ec20b7e83150ab2b
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-19 21:25:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,6 +16,8 @@ class Application
     elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
+      end
+    elsif req.path.match(/add/)
     else
       resp.write "Path Not Found"
     end
