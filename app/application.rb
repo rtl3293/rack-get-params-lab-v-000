6f09d71cb84efccfe0fbec172f72cc23afcb4889
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-19 21:32:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,6 +16,7 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
+      else
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
