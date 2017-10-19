0f1340c7feaa28110b83293c5bb4c55f8c58a17a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-19 21:25:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
     else
       resp.write "Path Not Found"
     end
